500845872693d5b14513ac16f640e98acb08f63a
Jesús Ángel Samitier <jasamitier@funiglobal.com>
2019-01-05 11:59:48 +0100

    webpack config by environment

--- webpack.config.js
+++ webpack.config.js
@@ -3,92 +3,98 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].[chunkhash].js'
-    },
-    //devtool: 'source-map',
-    devtool: 'inline-source-map',
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            }),
-            new OptimizeCSSAssetsPlugin({})
-        ]
-    },
-    module: {
-        rules: [{
-                test: /\.tag\.html$/,
-                exclude: /node_modules/,
-                loader: 'riot-tag-loader',
-                query: {
-                    type: 'es6', // transpile the riot tags using babel
-                    hot: true
-                }
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+
+module.exports = (env) => {
+    const devMode = env !== 'prod';
+    let config = {};
+    if (devMode) {
+        config.devtool = 'inline-source-map';
+    } else {
+        config.optimization = {
+            minimizer: [
+                new TerserPlugin(),
+                new OptimizeCSSAssetsPlugin()
+            ]
+        }
+    }
+    config = {
+        ...config,
+        entry: {
+            main: './src/index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].[chunkhash].js'
+        },
+
+        module: {
+            rules: [{
+                    test: /\.tag\.html$/,
+                    exclude: /node_modules/,
+                    loader: 'riot-tag-loader',
+                    query: {
+                        type: 'es6',
+                        hot: true
                     }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: 'css-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.(sass|scss)$/,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: 'css-loader',
+                }, {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
                         options: {
-                            minimize: true
+                            presets: ['@babel/preset-env']
                         }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                    },
-                    {
-                        loader: 'sass-loader'
                     }
-                ]
-            },
+                },
+                {
+                    test: /\.css$/,
+                    use: [{
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                        }
+                    ]
+                },
+                {
+                    test: /\.(sass|scss)$/,
+                    use: [{
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(path.resolve(__dirname, './dist'), {
+                root: process.cwd()
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.[contenthash].css'
+            }),
+            new HtmlWebpackPlugin({
+                inject: false,
+                hash: true,
+                template: './src/index.html',
+                filename: 'index.html'
+            }),
+            new WebpackMd5Hash()
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(path.resolve(__dirname, './dist'), {
-            root: process.cwd()
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
-        }),
-        new WebpackMd5Hash()
-    ]
+    }
+    return config;
 };
